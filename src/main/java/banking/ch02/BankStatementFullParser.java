d67becd21407b8404a42d8517a558062501411ce
jay <jay@within.co.kr>
2021-04-13 18:00:01 +0900

    Initial commit

---
+++ src/main/java/banking/ch02/BankStatementFullParser.java
@@ -0,0 +1,18 @@
+package banking.ch02;
+
+public class BankStatementFullParser {
+    BankTransaction parseFromCSV(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    BankTransaction parseFromJSON(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+
+    BankTransaction parseFromXML(final String line) {
+        // ...
+        throw new UnsupportedOperationException();
+    }
+}
